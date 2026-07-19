a664d6f3286a038c500e85e0b1f9ad974ae327ba
Alex Dixon <alex@dixonary.co.uk>
2026-07-19 15:52:39 +0100

    Add visually distinct icons

--- public/mfds.js
+++ public/mfds.js
@@ -163,6 +163,36 @@ const initialiseSidebar = () => {
 }
 
 //**************************************************//
+// SENDER ICONS
+
+let show_sender_icons = false;
+
+const toggleSenderIcons = () => {
+  show_sender_icons = !show_sender_icons;
+  updateSenderIcons();
+}
+
+const updateSenderIcons = () => {
+  const main = $("main");
+
+  if (show_sender_icons) {
+    main.classList.add("show-sender-icons");
+  }
+  else {
+    main.classList.remove("show-sender-icons");
+  }
+
+  localStorage.setItem("show-sender-icons", show_sender_icons);
+}
+
+const initialiseSenderIcons = () => {
+  const os = JSON.parse(localStorage.getItem("show-sender-icons"));
+  show_sender_icons = !!os;
+  console.log(`Sender icons initialised to ${show_sender_icons ? "visible" : "hidden"}`);
+  updateSenderIcons();
+}
+
+//**************************************************//
 // DICTIONARY
 
 const updateDict = () => {
@@ -257,7 +287,7 @@ const updateEncryptionKeys = () => {
   const elKeys = $(".encryption-keys");
   const elNum = $(".encryption-keys .num-encryption-keys");
   const elAll = $("#all-encryption-keys");
-  elNum.innerText = `${n} KEY${n == 1 ? "" : "S"} IN USE`;
+  elNum.innerText = `${n} KEY${n == 1 ? "" : "S"} ENABLED`;
   elKeys.setAttribute("data-n", n);
 
   elAll.innerHTML = "";
@@ -309,6 +339,10 @@ const getColor = (value) => {
   const hue = (137.5 * value) % 360;
   return `hsl(${hue}deg 100% 70%)`;
 }
+const getColorRGB = (value) => {
+  const hue = (137.5 * value) % 360;
+  return hslToHex(hue, 100, 70);
+}
 
 const updateLocalCallSign = () => {
   const elems = $$(".digit .value");
@@ -342,7 +376,9 @@ const randomizeCallSign = () => {
 
 const setActiveCallSigns = (n) => {
   activeCallSigns = n;
-  $(".num-call-signs").innerText = `${n} CALL SIGN${n == 1 ? '' : 'S'} ACTIVE`;
+  const numCallSigns = $(".num-call-signs");
+  numCallSigns.innerHTML = jdenticon.toSvg("num call signs", 200, { backColor: getColorRGB(12345), lightness: { color: [0, 1.0], grayscale: [0, 1.0] } });
+  $(".num-call-signs").innerHTML += `${n} CALL SIGN${n == 1 ? '' : 'S'} ACTIVE`;
 }
 
 const renderCallSign = (n) => {
@@ -503,6 +539,13 @@ const renderMessage = (sender, sequence, message, encryptionKey) => {
 
   const el = document.createElement("div");
   el.classList.add("message");
+
+  const iconEl = document.createElement("div");
+  iconEl.classList.add("sender-icon");
+  // iconEl.innerHTML = `<svg data-jdenticon-value="${sender}"></svg>`;
+  console.log(getColorRGB(sender));
+  iconEl.innerHTML = jdenticon.toSvg(sender, 200, { backColor: getColorRGB(sender ^ 3) + "aa", lightness: { color: [0, 1.0], grayscale: [0, 1.0] } });
+  el.appendChild(iconEl);
 
   const mel = document.createElement("div");
   mel.classList.add("message-body");
@@ -678,7 +721,6 @@ const renderMessage = (sender, sequence, message, encryptionKey) => {
   $("#all-messages").appendChild(el);
 
   scrollToBottom();
-
 }
 
 const renderErrorMessage = (message, classes = ["error"]) => {
@@ -1377,7 +1419,47 @@ window.onload = () => {
 
     const clipboardDialog = $("dialog.clipboard-paste");
     clipboardDialog.close();
-
   });
 
+  initialiseSenderIcons();
+  $(".num-call-signs").addEventListener("click", () => {
+    toggleSenderIcons();
+  });
+}
+
+
+
+
+//************************************************//
+// HELPERS
+
+
+// From https://www.jameslmilner.com/posts/converting-rgb-hex-hsl-colors/
+function hslToHex(h, s, l) {
+  h /= 360;
+  s /= 100;
+  l /= 100;
+  let r, g, b;
+  if (s === 0) {
+    r = g = b = l; // achromatic
+  } else {
+    const hue2rgb = (p, q, t) => {
+      if (t < 0) t += 1;
+      if (t > 1) t -= 1;
+      if (t < 1 / 6) return p + (q - p) * 6 * t;
+      if (t < 1 / 2) return q;
+      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+      return p;
+    };
+    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+    const p = 2 * l - q;
+    r = hue2rgb(p, q, h + 1 / 3);
+    g = hue2rgb(p, q, h);
+    b = hue2rgb(p, q, h - 1 / 3);
+  }
+  const toHex = x => {
+    const hex = Math.round(x * 255).toString(16);
+    return hex.length === 1 ? '0' + hex : hex;
+  };
+  return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
 }
